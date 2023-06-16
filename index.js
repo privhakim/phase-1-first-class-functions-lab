3cd7478cead7e717ccab3e10d933682fa0bebb74
privhakim <abdihakim.hassan@student.moringaschool.com>
2023-06-16 22:15:18 +0300

    done work

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function() {
+ return  drivers.slice(0, 1);
+}
+
+const returnLastTwoDrivers = function() {
+    return drivers.slice(2, 3);
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+const fareMultiplier = function(Multiplier) {
+    return function(fare) {mkjuj
+    return fare* multiplier;
+    };
+}
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+const selectDifferentDrivers = function(drivers, driversSelector) {
+    return driversSelector(drivers);
+};
